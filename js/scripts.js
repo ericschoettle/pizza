8c2280297c9e3615da7c764e5aa8374ea74a9ef3
ericschoettle <eric.schoettle@gmail.com>
2017-04-07 09:40:28 -0700

    add sizes

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,13 @@ function Pizza(size, crust, toppings) {
   this.toppings = toppings;
   this.price = NaN;
 }
+debugger
+var small = {diameter: 8, price: 10}
+var medium = {diameter: 12, price: 14}
+var large = {diameter: 18, price: 19}
+var xl = {diameter: 23, price: 24}
 
+var sizes = {"small":small, "medium":medium, "large":large, "xl":xl, }
 
 // Front End
 $(document).ready(function() {
