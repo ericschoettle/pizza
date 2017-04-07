9b0083b8fde27d3bf816d622bb5a5b149848d926
ericschoettle <eric.schoettle@gmail.com>
2017-04-07 09:27:43 -0700

    add pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
+// Back End
+function Pizza(size, crust, toppings) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.price = NaN;
+}
+
+
+// Front End
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    
+
   })
 });
